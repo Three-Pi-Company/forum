ca45e2b20ab88022da7db69468872b3cdde080a5
MacPal2002 <maciek.palenica@outlook.com>
2023-01-12 22:58:26 +0100

    fixes

--- sign-up.php
+++ sign-up.php
@@ -244,14 +244,7 @@
             return false;
         }
     }
-    
-    grecaptcha.ready(function() {
-        grecaptcha.execute('<?php echo SITE_KEY; ?>', {action: 'homepage'})
-        .then(function(token) {
-            //console.log(token);
-            document.getElementById('g-recaptcha-response').value=token;
-        });
-    });
+
 </script>
 </body> 
 </html>
